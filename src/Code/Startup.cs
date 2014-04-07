ea2b00370f5dfee1f42e111eda8e9377d3ec7d33
Wesley McClure <wes.mcclure@gmail.com>
2014-04-07 10:01:12 -0700

    Revert "add world - demo promote"
    This reverts commit 62b6e5511fe82c70ffc37b779d823e03cbd049a1.

--- src/Code/Startup.cs
+++ src/Code/Startup.cs
@@ -6,7 +6,7 @@ namespace Project
     {
         public void Configuration(IAppBuilder app)
         {
-            app.UseHandler((req, res) => res.WriteAsync("Hello World"));
+            app.UseHandler((req, res) => res.WriteAsync("Hello "));
         }
     }
 }
